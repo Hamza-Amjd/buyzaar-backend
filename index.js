b7b80d46819fe497c17b67bea637d29eb6f44a43
Hamza <135211820+Hamza-Amjd@users.noreply.github.com>
2024-03-26 17:05:41 +0500

    deploymentfix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 connectToMongo();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
